afdeb3bc88f1cabf94c2af0e714bc6322acd80d9
Thomas Sarafian <thomassarafian@gmail.com>
2021-03-20 14:45:09 +0100

    CRUD works

---
+++ app/controllers/sneakers_controller.rb
@@ -0,0 +1,43 @@
+class SneakersController < ApplicationController
+	before_action :set_sneaker, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@sneakers = Sneaker.all
+	end
+	
+	def show
+	end
+	
+	def new
+		@sneaker = Sneaker.new
+	end
+	
+	def create
+		sneaker = Sneaker.create(sneaker_params)
+		redirect_to sneakers_path
+	end
+
+	def edit
+	end
+	
+	def update
+		@sneaker.update(sneaker_params)
+		redirect_to sneakers_path
+	end
+
+	def destroy
+		@sneaker.destroy
+		redirect_to sneakers_path
+	end
+
+	private
+
+	def sneaker_params
+		params.require(:sneaker).permit(:name, :size, :price, :condition, :serial_number, :box, :extras)
+	end
+
+	def set_sneaker
+		@sneaker = Sneaker.find(params[:id])
+	end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Sneaker.create(name: "Nike Air Presto \"Racer Blue\"", size: 44.5, price: 199, condition: 5, serial_number: "8323923892", box: "OG Box", extras: "lacets")
+Sneaker.create(name: "Nike Air Max 90 \"Football\"", size: 42, price: 125, condition: 8, serial_number: "48374287284", box: "OG Box", extras: "lacets")
+Sneaker.create(name: "adidas NMD R1 PK \"Footwear White\"", size: 38, price: 149, condition: 10, serial_number: "982932932", box: "OG Box", extras: "lacets")
