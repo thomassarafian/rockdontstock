e542f73dbf2ddb27535e214b4d714b5d52b1ecb1
Thomas Sarafian <thomassarafian@gmail.com>
2021-03-25 07:52:18 +0100

    Configure strong params for users in ApplicationController

--- db/migrate/20210323092726_add_first_name_to_users.rb
+++ db/migrate/20210323092726_add_first_name_to_users.rb
@@ -1,9 +1,3 @@
-# class RenameUserFirstnameToFirstName < ActiveRecord::Migration[6.1]
-#   def change
-# 	rename_column :users, :firstname, :first_name
-#   end
-# end
-
 class AddFirstNameToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :first_name, :string
