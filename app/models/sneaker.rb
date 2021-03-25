5d3ed1b4cd214a111efca99be4d6517e1f5cd11c
Thomas Sarafian <thomassarafian@gmail.com>
2021-03-25 08:57:17 +0100

    User can uplodad multiple photos thanks to Cloundinary

--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -1,4 +1,5 @@
 class Sneaker < ApplicationRecord
+	has_many_attached :photos
 	belongs_to :user
 	validates :size, :price, :condition, :serial_number, presence: true
 	#Reflechir sur extras, box et name 
