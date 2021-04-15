55acb44891adcfd72b3e0c3510dac687832c1c7f
Thomas Sarafian <thomassarafian@gmail.com>
2021-04-15 10:10:32 +0200

    production no impact change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,6 +119,6 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
 
-  # config.action_mailer.default_url_options = { host: 'rockdontstock.com' }
+  config.action_mailer.default_url_options = { host: 'rockdontstock.com' }
   
 end
