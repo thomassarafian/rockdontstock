ec0420f5629063cf2b89f9c98ef12a3bce3cb446
Thomas Sarafian <thomassarafian@gmail.com>
2021-04-19 09:37:47 +0200

    Initalize account stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,8 @@
 # Stripe.api_key = ENV['MAILCHIMPTRANS_API_KEY']
+# Set your secret key. Remember to switch to your live secret key in production.
+# See your keys here: https://dashboard.stripe.com/apikeys
+Stripe.api_key = ENV["STRIPE_SECRET"]
+
+account = Stripe::Account.create({
+  type: 'express',
+})
