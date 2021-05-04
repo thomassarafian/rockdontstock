13696414afed44021a2dfda2f6270d78efc408de
Thomas Sarafian <thomassarafian@gmail.com>
2021-05-04 21:39:10 +0200

    Add a new controller Users in order to create my profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
 	root to: "pages#home"
-  	devise_for :users, controllers: { 
-  		omniauth_callbacks: 'users/omniauth_callbacks',
-  		registrations: 'users/registrations'
-  	}
+	devise_for :users, controllers: { 
+		omniauth_callbacks: 'users/omniauth_callbacks',
+		registrations: 'users/registrations'
+	}
 	resources :sneakers
+	resources :users
 	resources :orders, only: [:show, :create] do
 	  resources :payments, only: :new
 	end
