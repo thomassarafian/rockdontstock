01e21db6405eadd5301019f8ca29cd71fa7b8153
Thomas Sarafian <thomassarafian@gmail.com>
2021-05-06 22:55:03 +0200

    retrieve the balance of a connected account

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -2,6 +2,11 @@ class TransfersController < ApplicationController
 	def index
 		skip_policy_scope
 		@account = Stripe::Account.retrieve('acct_1InK1r2QFklsr9vG')
+		@balance = Stripe::Balance.retrieve({
+			stripe_account: 'acct_1InK1r2QFklsr9vG'
+		})
+
+		# raise
 		# @bank_account = Stripe::Account.create_external_account(
 		# 	'acct_1InK1r2QFklsr9vG',
 		# 	{
@@ -17,15 +22,15 @@ class TransfersController < ApplicationController
 		# )
 
 		# raise
-		payout = Stripe::Payout.create({
-		  amount: 100,
-		  currency: 'eur',
-		  destination: 'ba_1Inmn42QFklsr9vGI323QckG',#@account.external_accounts.data[0].id
-		  source_type: 'bank_account',
-		  method: "standard",
-			}, {
-				stripe_account: 'acct_1InK1r2QFklsr9vG',
-			})
+		# payout = Stripe::Payout.create({
+		#   amount: 100,
+		#   currency: 'eur',
+		#   destination: 'ba_1Inmn42QFklsr9vGI323QckG',#@account.external_accounts.data[0].id
+		#   source_type: 'bank_account',
+		#   method: "standard",
+		# 	}, {
+		# 		stripe_account: 'acct_1InK1r2QFklsr9vG',
+		# 	})
 		# payout = Stripe::Payout.create({
 		#   amount: 1000,
 		#   currency: 'eur',
