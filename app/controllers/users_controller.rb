a6002f26251fcb7fa4452eb63e476abe06fa4884
Thomas Sarafian <thomassarafian@gmail.com>
2021-05-27 18:40:08 +0200

    User can put his adress and it will display without manually reloading the page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,8 @@ class UsersController < ApplicationController
 	end
 	
 	def update
-		@user.update(user_params)
-		if @user.save!
-			  render 'update'
+		if @user.update(user_params)
+			render 'update'
 			# redirect_to user_path(@user) # je ne connais pas les conséquences de ça, j'ai commenté pour pouvoir modifier avec remote : true dans payment#new l'adresse
 		end
 
