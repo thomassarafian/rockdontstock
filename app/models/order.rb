a85a0ef814ff761c47d67c69e60af02f5984bb58
Thomas Sarafian <thomassarafian@gmail.com>
2021-05-30 17:38:45 +0200

    add service %

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,23 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :sneaker
+
   monetize :price_cents
   monetize :insurance_cents
   monetize :shipping_cost_cents
+  monetize :service_cents
+
 
   after_create :shipping_price
 
+
+  def percent_of(a, n)
+    a.to_f / n.to_f * 100.0
+  end
+
+
   def shipping_price
-  	case self.sneaker.price_cents / 100
+    case self.sneaker.price_cents / 100
   	when 50..124
   		  self.insurance_cents = 200
   	when 125..249
@@ -21,7 +30,16 @@ class Order < ApplicationRecord
 			self.insurance_cents = 800
   	end
   	self.shipping_cost_cents = 490
-    SendcloudCreateLabel.new(user, self).create_label
+
+    p self.sneaker.price_cents
+    p self.sneaker.price_cents
+    p self.sneaker.price_cents
+
+    self.service_cents = (percent_of(12, self.sneaker.price_cents / 100))
+    
+    # raise
+    
+    # SendcloudCreateLabel.new(user, self).create_label
   end
 
 end
