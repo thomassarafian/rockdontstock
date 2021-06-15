a397fc8a9214167c3681880cf3c24654262e2aa6
Thomas Sarafian <thomassarafian@gmail.com>
2021-06-15 10:47:29 +0200

    sneaker can be created before login with google or facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
+    
+    if session[:sneaker_session_id]
+      @sneaker_session = Sneaker.where(id: session[:sneaker_session_id])
+      @sneaker_session[0].update(user_id: user.id)
+      @sneaker_session[0].save
+    end
 
     if user.persisted?
       sign_in_and_redirect user, event: :authentication
@@ -13,6 +19,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     user = User.create_from_google_data(request.env['omniauth.auth'])
+
+    if session[:sneaker_session_id]
+      @sneaker_session = Sneaker.where(id: session[:sneaker_session_id])
+      @sneaker_session[0].update(user_id: user.id)
+      @sneaker_session[0].save
+    end
 
     if user.persisted?
       sign_in_and_redirect user, event: :authentication
