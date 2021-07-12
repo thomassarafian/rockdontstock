4b764fbfd2e1efcf91a2c6e88d7dd81b10bac2fd
Thomas Sarafian <thomassarafian@gmail.com>
2021-07-12 13:49:58 +0200

    clean algolia autcomplete adress in javascript/packs

---
+++ app/javascript/packs/algolia_autocomplete_address.js
@@ -0,0 +1,14 @@
+let placesAutocomplete = places({
+      appId: process.env.ALGOLIA_APP_ID,
+      apiKey: process.env.ALGOLIA_API_KEY,
+      container: document.querySelector('#user_line1'),
+      language: 'fr', 
+      countries: ['fr'],
+      type: 'address'
+    });
+    
+placesAutocomplete.on('change', function(e) {
+  $('input[type=hidden]#user_city').val(e.suggestion.city);
+  $('input[type=hidden]#user_postal_code').val(e.suggestion.postcode);
+  $('input[type=hidden]#user_line1').val(e.suggestion.name);
+});
