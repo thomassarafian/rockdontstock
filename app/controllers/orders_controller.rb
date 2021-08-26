cb9bafb34c4e9b5e974b6a523d061327c6b3017a
Thomas Sarafian <thomassarafian@gmail.com>
2021-08-26 16:54:17 +0200

    fix css

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+
 	def show
 		# On créé la commande 
 		# On envoie l'email au vendeur
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,6 +16,7 @@ class Users::SessionsController < Devise::SessionsController
       @sneaker_session[0].update(user_id: current_user.id)
       @sneaker_session[0].save
     end
+
   end
 
   # DELETE /resource/sign_out
@@ -24,6 +25,9 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # protected
+  
+  # def after_sign_in_path_for(resource)
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
