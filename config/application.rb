e0d0800f7219e0c94ae49013fc9ae0cb12335856
Thomas Sarafian <thomassarafian@gmail.com>
2021-08-27 22:00:45 +0200

    delete redis for production

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module Rockdontstock
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.active_job.queue_adapter = :sidekiq
 
     config.i18n.default_locale = :fr
     null_regex = Regexp.new(/\Anull\z/)
