e0286d02629465d929d75fa366f7cfe30ecf472d
Thomas Sarafian <thomassarafian@gmail.com>
2021-08-27 21:44:25 +0200

    process add new sneaker is clean

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -25,33 +25,52 @@ class SneakersController < ApplicationController
     if params['sneaker_db_id'].present?
       @sneaker_db = SneakerDb.find(params['sneaker_db_id'])
     end
-    if current_user == nil
-      reset_session
-      @sneaker = Sneaker.new(user_id: 1,
+    if !params['button-new-sneaker'].present?
+      if current_user == nil
+        reset_session
+        @sneaker = Sneaker.new(user_id: 1,
         size: params['sneaker']['size'], condition: params['sneaker']['condition'], 
         price: params['sneaker']['price'], photos: params['sneaker']['photos'],
         box: params['sneaker']['box'], extras: params['sneaker']['extras'],
         state: 0, sneaker_db_id: @sneaker_db.id)
-        if !@sneaker.save
-          render :new
-        else
-          session[:sneaker_session_id] = @sneaker.id
-          redirect_to new_user_session_path, notice: "Tu dois être connecté pour que nous puissions vérifier ta paire"
-        end
-    elsif user_signed_in?
-      @sneaker = current_user.sneakers.new(sneaker_db_id: @sneaker_db.id)
-      @sneaker.update(sneaker_params)
-   		authorize @sneaker
-  		if @sneaker.save
-  			@sneaker.update(state: 0) # ici on devrai laisser à 0, puis si on valide la paire cote admin, alors on la passera a 1 
-        if user_signed_in? && current_user.date_of_birth? && current_user.line1? && current_user.city? && current_user.postal_code? && current_user.phone?
-  				redirect_to sneaker_path(@sneaker), notice: "Ta paire a bien été envoyé !"
-  			else
-  				# Redirection vers son compte pour pouvoir y apporter les modifications + pop up pour indiquer que sa chaussure a bien ete submit
-  				redirect_to edit_user_registration_path, notice: "Ta paire a bien été envoyé ! Complète ton adresse, ta date de naissance et ton numéro de téléphone pour que des acheteurs puissent te l'acheter"
-  				#on créé pas automatiquement un compte connect ici car en realité si cote admin on valide la paire + le compte on créra un compte connect
-  			end
-  		end
+        @sneaker.save!
+        session[:sneaker_session_id] = @sneaker.id
+      elsif user_signed_in?
+        @sneaker = current_user.sneakers.new(sneaker_db_id: @sneaker_db.id)
+        @sneaker.update(sneaker_params)
+        @sneaker.update(state: 0)
+        authorize @sneaker
+        @sneaker.save!
+      end  
+    else
+      if current_user == nil  
+        # reset_session
+        # @sneaker = Sneaker.new(user_id: 1,
+        #   size: params['sneaker']['size'], condition: params['sneaker']['condition'], 
+        #   price: params['sneaker']['price'], photos: params['sneaker']['photos'],
+        #   box: params['sneaker']['box'], extras: params['sneaker']['extras'],
+        #   state: 0, sneaker_db_id: @sneaker_db.id)
+        #   if !@sneaker.save
+        #     render :new
+        #   else
+            # session[:sneaker_session_id] = @sneaker.id
+            redirect_to new_user_session_path, notice: "Tu dois être connecté pour que nous puissions vérifier ta paire"
+          # end
+      elsif user_signed_in?
+      #   @sneaker = current_user.sneakers.new(sneaker_db_id: @sneaker_db.id)
+      #   @sneaker.update(sneaker_params)
+     	# 	authorize @sneaker
+    		# if @sneaker.save
+    		# 	@sneaker.update(state: 0) # ici on devrai laisser à 0, puis si on valide la paire cote admin, alors on la passera a 1 
+          if user_signed_in? && current_user.date_of_birth? && current_user.line1? && current_user.city? && current_user.postal_code? && current_user.phone?
+    				redirect_to sneaker_path(current_user.sneakers.last), notice: "Ta paire a bien été envoyé !"
+    			else
+    				# Redirection vers son compte pour pouvoir y apporter les modifications + pop up pour indiquer que sa chaussure a bien ete submit
+    				redirect_to edit_user_registration_path, notice: "Ta paire a bien été envoyé ! Complète ton adresse, ta date de naissance et ton numéro de téléphone pour que des acheteurs puissent te l'acheter"
+    				#on créé pas automatiquement un compte connect ici car en realité si cote admin on valide la paire + le compte on créra un compte connect
+    			end
+    		# end
+      end
     end
 	end
 
