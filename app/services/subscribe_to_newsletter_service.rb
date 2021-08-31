955c2a6c1c6a940ee71abc9c4250101796efa074
Thomas Sarafian <thomassarafian@gmail.com>
2021-08-31 17:25:43 +0200

    little fix

--- app/services/subscribe_to_newsletter_service.rb
+++ app/services/subscribe_to_newsletter_service.rb
@@ -25,16 +25,24 @@ class SubscribeToNewsletterService
   end
 
   def call
-    @gibbon.lists(@audience_id).members.create(
-      body: {
-        email_address: @user.email,
-        status: "subscribed",
-        merge_fields: {
-          PRENOM: @user.first_name,
-          NOM: @user.last_name
+    begin
+      @gibbon.lists(@audience_id).members.create(
+        body: {
+          email_address: @user.email,
+          status: "subscribed",
+          merge_fields: {
+            PRENOM: @user.first_name,
+            NOM: @user.last_name
+          }
         }
-      }
-    )
+      )
+    rescue  Gibbon::MailChimpError => e
+      if e.title == "Member Exists"
+        return "Tu es dejà inscrit à notre newsletter"
+      # else
+      #   return  "Félicitation ! Tu vas bientôt recevoir nos offres"
+      end
+    end
   end
 end
 
