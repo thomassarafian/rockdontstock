2da1970f21d0477e1287652b40f581926e974b70
nayow <nayow@hotmail.fr>
2021-12-03 00:14:05 +0100

    rollbacked before prettier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,17 @@
 class ApplicationController < ActionController::Base
-  include Pagy::Backend
-  include Pundit
+	include Pagy::Backend
+	include Pundit
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	before_action :authenticate_user!
-  before_action :set_search_navbar, unless: :skip_set_search_navbar?
+
+	# before_action :set_search_navbar, unless: :skip_set_search_navbar?
 
 	# Pundit: white-list approach.
 	after_action :verify_authorized, except: :index, unless: :skip_pundit?
 	after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
 	protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+
 	# Uncomment when you *really understand* Pundit!
 	# rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 	# def user_not_authorized
@@ -17,58 +19,55 @@ class ApplicationController < ActionController::Base
 	#   redirect_to(root_path)
 	# end
 
-
 	protected
 
-  def skip_set_search_navbar?
-    if (params[:controller] == "sneaker_dbs" && params[:action] == "index") || params["_method"] == "patch"
-      return true
-    else
-      return false
-    end
-  end
+	def skip_set_search_navbar?
+		if (params[:controller] == 'sneaker_dbs' && params[:action] == 'index') || params['_method'] == 'patch'
+			return true
+		else
+			return false
+		end
+	end
 
-  
-  def set_search_navbar
-    if Rails.env.production?
-      @pagy, @sneakers_navbar = pagy(Sneaker.includes(:sneaker_db, :user, :photos_attachments, photos_attachments: :blob).where("state >= ?", 1), items: 120)
-    elsif Rails.env.development?
-      @pagy, @sneakers_navbar = pagy(Sneaker.includes(:sneaker_db, :user, :photos_attachments, photos_attachments: :blob).where("state >= ?", 1), items: 120)
-    end
-      if params[:category].present? || params[:price].present? || params[:condition].present? || params[:size].present?
-        session[:filter_params] = params
-        filtering_params(params).each do |key, value|
-          @sneakers_navbar = @sneakers_navbar.public_send("filter_by_#{key}", value) if value.present?
-        end
-      elsif params[:page].present? && params[:page] >= "2" && session[:filter_params].present?
-        filtering_params(session[:filter_params]).each do |key, value|
-          @sneakers_navbar = @sneakers_navbar.public_send("filter_by_#{key}", value) if value.present?
-        end
-      elsif !params[:category].present? || !params[:price].present? || !params[:condition].present? || !params[:size].present?
-        session.delete(:filter_params)
-      end
-      if params[:query].present?
-        @sneakers_navbar = @sneakers_navbar.search_by_name_and_brand(params[:query])
-      end
+	def set_search_navbar
+		if Rails.env.production?
+			@pagy, @sneakers_navbar = pagy(Sneaker.includes(:sneaker_db, :user, :photos_attachments, photos_attachments: :blob).where('state >= ?', 1), items: 120)
+		elsif Rails.env.development?
+			@pagy, @sneakers_navbar = pagy(Sneaker.includes(:sneaker_db, :user, :photos_attachments, photos_attachments: :blob).where('state >= ?', 1), items: 120)
+		end
+		if params[:category].present? || params[:price].present? || params[:condition].present? || params[:size].present?
+			session[:filter_params] = params
+			filtering_params(params).each { |key, value| @sneakers_navbar = @sneakers_navbar.public_send("filter_by_#{key}", value) if value.present? }
+		elsif params[:page].present? && params[:page] >= '2' && session[:filter_params].present?
+			filtering_params(session[:filter_params]).each { |key, value| @sneakers_navbar = @sneakers_navbar.public_send("filter_by_#{key}", value) if value.present? }
+		elsif !params[:category].present? || !params[:price].present? || !params[:condition].present? || !params[:size].present?
+			session.delete(:filter_params)
+		end
+		@sneakers_navbar = @sneakers_navbar.search_by_name_and_brand(params[:query]) if params[:query].present?
 
-      respond_to do |format|
-        format.html
-        format.json 
-        format.text { render partial: 'shared/list.html.erb', locals: { sneakers: @sneakers_navbar, params: params}, pagination: view_context.pagy_nav(@pagy) }
-      end
-  end
+		respond_to do |format|
+			format.html
+			format.json
+			format.text { render partial: 'sneakers/list_sneakers.html.erb', locals: { sneakers: @sneakers_navbar, params: params }, pagination: view_context.pagy_nav(@pagy) }
+		end
+	end
 
-  def filtering_params(params)
-    params.slice(:price, :condition, :size, :category)
-  end
-
+	def filtering_params(params)
+		params.slice(:price, :condition, :size, :category)
+	end
 
 	def configure_permitted_parameters
 		# For additional fields in app/views/devise/registrations/new.html.erb
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :token_account, :token_person, :stripe_account_id, :person_id, :customer_id, :date_of_birth, :'date_of_birth(3i)', :'date_of_birth(2i)', :'date_of_birth(1i)', :line1, :city, :postal_code, :phone, :iban])
+		devise_parameter_sanitizer.permit(
+			:sign_up,
+			keys: [:first_name, :last_name, :token_account, :token_person, :stripe_account_id, :person_id, :customer_id, :date_of_birth, :'date_of_birth(3i)', :'date_of_birth(2i)', :'date_of_birth(1i)', :line1, :city, :postal_code, :phone, :iban],
+		)
 
 		# For additional in app/views/devise/registrations/edit.html.erb
-		devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :token_account, :token_person, :stripe_account_id, :person_id, :customer_id, :date_of_birth, :'date_of_birth(3i)', :'date_of_birth(2i)', :'date_of_birth(1i)', :line1, :city, :postal_code, :phone, :iban, ids: []])
+		devise_parameter_sanitizer.permit(
+			:account_update,
+			keys: [:first_name, :last_name, :token_account, :token_person, :stripe_account_id, :person_id, :customer_id, :date_of_birth, :'date_of_birth(3i)', :'date_of_birth(2i)', :'date_of_birth(1i)', :line1, :city, :postal_code, :phone, :iban, ids: []],
+		)
 	end
 
 	private
--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,113 +1,135 @@
 class SneakersController < ApplicationController
-	skip_before_action :authenticate_user!, only: [:index, :show, :new, :create]
-  skip_after_action :verify_authorized, only: [:new, :create]
+	skip_before_action :authenticate_user!, only: %i[index show new create]
+	skip_after_action :verify_authorized, only: %i[new create]
 
-	before_action :set_sneaker, only: [:show, :edit, :update, :destroy]
-		
+	before_action :set_sneaker, only: %i[show edit update destroy]
+
 	def index
-		@sneakers = policy_scope(Sneaker)
+		if Rails.env.production?
+			@pagy, @sneakers_navbar = pagy(Sneaker.includes(:sneaker_db, :user, :photos_attachments, photos_attachments: :blob).where('state >= ?', 1), items: 120)
+		elsif Rails.env.development?
+			@pagy, @sneakers_navbar = pagy(Sneaker.includes(:sneaker_db, :user, :photos_attachments, photos_attachments: :blob).where('state >= ?', 1), items: 120)
+		end
+		if params[:category].present? || params[:price].present? || params[:condition].present? || params[:size].present?
+			session[:filter_params] = params
+			filtering_params(params).each { |key, value| @sneakers_navbar = @sneakers_navbar.public_send("filter_by_#{key}", value) if value.present? }
+		elsif params[:page].present? && params[:page] >= '2' && session[:filter_params].present?
+			filtering_params(session[:filter_params]).each { |key, value| @sneakers_navbar = @sneakers_navbar.public_send("filter_by_#{key}", value) if value.present? }
+		elsif !params[:category].present? || !params[:price].present? || !params[:condition].present? || !params[:size].present?
+			session.delete(:filter_params)
+		end
+		@sneakers_navbar = @sneakers_navbar.search_by_name_and_brand(params[:query]) if params[:query].present?
+
+		respond_to do |format|
+			format.html
+			format.json
+			format.text { render partial: 'sneakers/list_sneakers.html.erb', locals: { sneakers: @sneakers_navbar, params: params }, pagination: view_context.pagy_nav(@pagy) }
+		end
 	end
-	
+
 	def show
-    @sneaker = Sneaker.find(params[:id])
+		@sneaker = Sneaker.find(params[:id])
 	end
-	
+
 	def new
-    if params['sneaker_db_id'].present?
-      @sneaker_db = SneakerDb.find(params['sneaker_db_id'])
-    end
-    # raise
-    # session[:sneaker_db_id] = params['sneaker_db_id'] 
+		@sneaker_db = SneakerDb.find(params['sneaker_db_id']) if params['sneaker_db_id'].present?
+
+		# raise
+		# session[:sneaker_db_id] = params['sneaker_db_id']
 		@sneaker = Sneaker.new
 		authorize @sneaker
 	end
-	
+
 	def create
-    if params['sneaker_db_id'].present?
-      @sneaker_db = SneakerDb.find(params['sneaker_db_id'])
-    end
-    if !params['button-new-sneaker'].present?
-      if current_user == nil
-        session.delete(:sneaker_session_id)
-        @sneaker = Sneaker.new(user_id: 1,
-        size: params['sneaker']['size'], condition: params['sneaker']['condition'], 
-        price: params['sneaker']['price'], photos: params['sneaker']['photos'],
-        box: params['sneaker']['box'], extras: params['sneaker']['extras'],
-        state: 0, sneaker_db_id: @sneaker_db.id)
-        @sneaker.save!
-        session[:sneaker_session_id] = @sneaker.id
-      elsif user_signed_in?
-        @sneaker = current_user.sneakers.new(sneaker_db_id: @sneaker_db.id)
-        @sneaker.update(sneaker_params)
-        @sneaker.update(state: 0)
-        authorize @sneaker
-        @sneaker.save!
-      end  
-    else
-      if current_user == nil  
-        # @sneaker = Sneaker.new(user_id: 1,
-        #   size: params['sneaker']['size'], condition: params['sneaker']['condition'], 
-        #   price: params['sneaker']['price'], photos: params['sneaker']['photos'],
-        #   box: params['sneaker']['box'], extras: params['sneaker']['extras'],
-        #   state: 0, sneaker_db_id: @sneaker_db.id)
-        #   if !@sneaker.save
-        #     render :new
-        #   else
-            # session[:sneaker_session_id] = @sneaker.id
-            redirect_to new_user_session_path, notice: "Tu dois être connecté pour que nous puissions vérifier ta paire"
-          # end
-      elsif user_signed_in?
-       #  @sneaker = current_user.sneakers.new(sneaker_db_id: @sneaker_db.id)
-       #  @sneaker.update(sneaker_params)
-     		# authorize @sneaker
-    		# if @sneaker.save
-    		# 	@sneaker.update(state: 0) # ici on devrai laisser à 0, puis si on valide la paire cote admin, alors on la passera a 1 
-          if user_signed_in? && current_user.date_of_birth? && current_user.line1? && current_user.city? && current_user.postal_code? && current_user.phone?
-    				redirect_to sneaker_path(current_user.sneakers.last), notice: "Ta paire a bien été envoyé !"
-    			else
-    				# Redirection vers son compte pour pouvoir y apporter les modifications + pop up pour indiquer que sa chaussure a bien ete submit
-    				redirect_to edit_user_registration_path, notice: "Ta paire a bien été envoyé ! Complète ton adresse, ta date de naissance et ton numéro de téléphone pour que des acheteurs puissent te l'acheter"
-    				#on créé pas automatiquement un compte connect ici car en realité si cote admin on valide la paire + le compte on créra un compte connect
-    			end
-          variable = Mailjet::Send.create(messages: [{
-            'From'=> {
-              'Email'=> "elliot@rockdontstock.com",
-              'Name'=> "Rock Don't Stock"
-            },
-            'To'=> [
-              {
-                'Email'=> current_user.email,
-                'Name'=> current_user.first_name,
-              }
-            ],
-            'TemplateID'=> 2961370,
-            'TemplateLanguage'=> true,
-            'Subject'=> "Ta paire est en cours de validation ⌛",
-            'Variables'=> {
-              "prenom" => current_user.first_name,
-              "modele_paire" => current_user.sneakers.last.sneaker_db.name
-            }
-          }])
-    		# end
-      end
-    end
+		@sneaker_db = SneakerDb.find(params['sneaker_db_id']) if params['sneaker_db_id'].present?
+		if !params['button-new-sneaker'].present?
+			if current_user == nil
+				session.delete(:sneaker_session_id)
+				@sneaker =
+					Sneaker.new(
+						user_id: 1,
+						size: params['sneaker']['size'],
+						condition: params['sneaker']['condition'],
+						price: params['sneaker']['price'],
+						photos: params['sneaker']['photos'],
+						box: params['sneaker']['box'],
+						extras: params['sneaker']['extras'],
+						state: 0,
+						sneaker_db_id: @sneaker_db.id,
+					)
+				@sneaker.save!
+				session[:sneaker_session_id] = @sneaker.id
+			elsif user_signed_in?
+				@sneaker = current_user.sneakers.new(sneaker_db_id: @sneaker_db.id)
+				@sneaker.update(sneaker_params)
+				@sneaker.update(state: 0)
+				authorize @sneaker
+				@sneaker.save!
+			end
+		else
+			if current_user == nil
+				# @sneaker = Sneaker.new(user_id: 1,
+				#   size: params['sneaker']['size'], condition: params['sneaker']['condition'],
+				#   price: params['sneaker']['price'], photos: params['sneaker']['photos'],
+				#   box: params['sneaker']['box'], extras: params['sneaker']['extras'],
+				#   state: 0, sneaker_db_id: @sneaker_db.id)
+				#   if !@sneaker.save
+				#     render :new
+				#   else
+				# session[:sneaker_session_id] = @sneaker.id
+				redirect_to new_user_session_path, notice: 'Tu dois être connecté pour que nous puissions vérifier ta paire'
+				# end
+			elsif user_signed_in?
+				#  @sneaker = current_user.sneakers.new(sneaker_db_id: @sneaker_db.id)
+				#  @sneaker.update(sneaker_params)
+				# authorize @sneaker
+				# if @sneaker.save
+				# 	@sneaker.update(state: 0) # ici on devrai laisser à 0, puis si on valide la paire cote admin, alors on la passera a 1
+				if user_signed_in? && current_user.date_of_birth? && current_user.line1? && current_user.city? && current_user.postal_code? && current_user.phone?
+					redirect_to sneaker_path(current_user.sneakers.last), notice: 'Ta paire a bien été envoyé !'
+				else
+					# Redirection vers son compte pour pouvoir y apporter les modifications + pop up pour indiquer que sa chaussure a bien ete submit
+					redirect_to edit_user_registration_path, notice: "Ta paire a bien été envoyé ! Complète ton adresse, ta date de naissance et ton numéro de téléphone pour que des acheteurs puissent te l'acheter"
+					#on créé pas automatiquement un compte connect ici car en realité si cote admin on valide la paire + le compte on créra un compte connect
+				end
+				variable =
+					Mailjet::Send.create(
+						messages: [
+							{
+								'From' => {
+									'Email' => 'elliot@rockdontstock.com',
+									'Name' => "Rock Don't Stock",
+								},
+								'To' => [{ 'Email' => current_user.email, 'Name' => current_user.first_name }],
+								'TemplateID' => 2_961_370,
+								'TemplateLanguage' => true,
+								'Subject' => 'Ta paire est en cours de validation ⌛',
+								'Variables' => {
+									'prenom' => current_user.first_name,
+									'modele_paire' => current_user.sneakers.last.sneaker_db.name,
+								},
+							},
+						],
+					)
+				# end
+			end
+		end
 	end
 
-	def edit
-	end
-	
+	def edit; end
+
 	def update
-    if @sneaker.state == 0
-  		@sneaker.update(sneaker_params)
-  		redirect_to sneaker_path(@sneaker)
-    else
-      redirect_to root_path, notice: "Tu ne pas pas modifié une paire déjà en ligne"
-    end
+		if @sneaker.state == 0
+			@sneaker.update(sneaker_params)
+			redirect_to sneaker_path(@sneaker)
+		else
+			redirect_to root_path, notice: 'Tu ne pas pas modifié une paire déjà en ligne'
+		end
 	end
 
 	def destroy
 		@sneaker.photos.purge
-    @sneaker.destroy
+		@sneaker.destroy
 		redirect_to user_items_path
 	end
 
