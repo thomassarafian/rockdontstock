cee76ce61cd464cb35a87a91f8f1cab519306d85
nayow <nayow@hotmail.fr>
2021-12-31 12:48:13 +0100

    WIP guides

--- app/controllers/forest/guides_controller.rb
+++ app/controllers/forest/guides_controller.rb
@@ -5,12 +5,14 @@ class Forest::GuidesController < ForestLiana::SmartActionsController
 
     attrs = req.body.dig('data', 'attributes', 'values')
     file = attrs['File']
+    puts "*"*100, params
+    puts "*"*100, req.body
     puts "*"*100, file
-    guide.file.attach({
-      io: File.open(file),
-      filename: "guide.pdf",
-      content_type: "application/pdf"
-    })
+    # guide.file.attach({
+    #   io: File.open(file),
+    #   filename: "guide.pdf",
+    #   content_type: "application/pdf"
+    # })
 
     render json: { success: 'Guide uploadé avec succès.' }
   end
