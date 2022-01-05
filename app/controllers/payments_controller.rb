317f8619dd663be5961c8dc338d6927f6cc8ae1c
nayow <nayow@hotmail.fr>
2022-01-05 18:53:57 +0100

    skipped user auth for success page

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,5 @@
 class PaymentsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:lc_complete]
   
   def lc_stripe_checkout
     lc = Authentication.new(lc_request_params)
