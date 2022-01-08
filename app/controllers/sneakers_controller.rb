f0b2d37c05d732d18ddc0ae8fee954b9b658d899
nayow <nayow@hotmail.fr>
2022-01-08 16:47:46 +0100

    fixed sorting by on desktop

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,5 +1,5 @@
 class SneakersController < ApplicationController
-
+	skip_before_action :authenticate_user!, only: [:index, :show]
 	before_action :set_sneaker, only: [:show, :edit, :update, :destroy]
 
 	def index
