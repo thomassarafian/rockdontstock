f71aaaa3a9136b7ee7868a10e034af67e39c8e7c
Thomas Sarafian <thomassarafian@gmail.com>
2022-01-25 18:52:50 +0100

    leave this file like it was

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,11 @@ class PagesController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:home, :about, :newsletter, :faq, :cgv, :cgu, :authentication, :how_to_send_shoes, :trust_policy, :guide_request]
 	
   def home 
-    @sneakers_selected = []#Sneaker.includes(:sneaker_db, :user, :orders, :photos_attachments, photos_attachments: :blob).selected.limit(8)
-    @sneakers_selected = []#Sneaker.limit(8) if @sneakers_selected.empty?
+    @sneakers_selected = Sneaker.includes(:sneaker_db, :user, :orders, :photos_attachments, photos_attachments: :blob).selected.limit(8)
+    @sneakers_selected = Sneaker.limit(8) if @sneakers_selected.empty?
 
-    @sneakers_last_added = []#Sneaker.includes(:sneaker_db, :user, :orders, :photos_attachments, photos_attachments: :blob).where("state >= ?", 1).limit(8).order("created_at DESC")
-    @sneakers_last_added = []#Sneaker.limit(8) if @sneakers_last_added.empty?
+    @sneakers_last_added = Sneaker.includes(:sneaker_db, :user, :orders, :photos_attachments, photos_attachments: :blob).where("state >= ?", 1).limit(8).order("created_at DESC")
+    @sneakers_last_added = Sneaker.limit(8) if @sneakers_last_added.empty?
   end
   
   def newsletter
