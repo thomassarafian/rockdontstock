e9bdc7730dbd6d42bccb1781eff2c91de04ff04a
jonas <nayow@hotmail.fr>
2022-02-02 23:53:57 +0100

    uncommented emails

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -6,32 +6,32 @@ class Authentication < ApplicationRecord
   validates :first_name, :last_name, :email, :date_of_birth, :city, :newsletter, presence: true
   validates :newsletter, acceptance: true
 
-  # def send_information_email
-  #   photos_urls = self.photos.map{ |photo| photo.blob.url }.join("\n")
+  def send_information_email
+    photos_urls = self.photos.map{ |photo| photo.blob.url }.join("\n")
 
-  #   Mailjet::Send.create(messages: [{
-  #     'From'=> {
-  #       'Email'=> "hello@rockdontstock.com",
-  #       'Name'=> "Rock Don't Stock"
-  #     },
-  #     'To'=> [
-  #       {
-  #         'Email'=> "hello@rockdontstock.com",
-  #         'Name'=> "Rock Don't Stock"
-  #       }
-  #     ],
-  #     'TemplateID'=> 3482576,
-  #     'TemplateLanguage'=> true,
-  #     'Subject'=> "Nouvelle demande de Legit Check",
-  #     'Variables'=> {
-  #       "nom" => self.last_name,
-  #       "prenom" => self.first_name,
-  #       "email" => self.email,
-  #       "naissance" => self.date_of_birth,
-  #       "ville" => self.city,
-  #       "photos" => photos_urls
-  #     }
-  #   }])
-  # end
+    Mailjet::Send.create(messages: [{
+      'From'=> {
+        'Email'=> "hello@rockdontstock.com",
+        'Name'=> "Rock Don't Stock"
+      },
+      'To'=> [
+        {
+          'Email'=> "hello@rockdontstock.com",
+          'Name'=> "Rock Don't Stock"
+        }
+      ],
+      'TemplateID'=> 3482576,
+      'TemplateLanguage'=> true,
+      'Subject'=> "Nouvelle demande de Legit Check",
+      'Variables'=> {
+        "nom" => self.last_name,
+        "prenom" => self.first_name,
+        "email" => self.email,
+        "naissance" => self.date_of_birth,
+        "ville" => self.city,
+        "photos" => photos_urls
+      }
+    }])
+  end
 
 end
