56f92ec87121cd59d32168ff04d7be63070661bb
Thomas Sarafian <thomassarafian@gmail.com>
2025-10-22 20:20:10 +0200

    Update Cloudinary configuration to conditionally initialize based on active storage service

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,10 @@
-Cloudinary.config do |config|
-  config.cloud_name = ENV['CLOUDINARY_NAME']
-  config.api_key = ENV['CLOUDINARY_API_KEY']
-  config.api_secret = ENV['CLOUDINARY_SECRET_KEY']
-  config.secure = true
-  config.cdn_subdomain = true
+# Only configure Cloudinary if we're using the cloudinary service
+if Rails.application.config.active_storage.service == :cloudinary
+  Cloudinary.config do |config|
+    config.cloud_name = ENV['CLOUDINARY_NAME']
+    config.api_key = ENV['CLOUDINARY_API_KEY']
+    config.api_secret = ENV['CLOUDINARY_SECRET_KEY']
+    config.secure = true
+    config.cdn_subdomain = true
+  end
 end
